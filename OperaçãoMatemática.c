25dbb52f2cb4cd197ed369f3a30b890126af3d65
Zeus Xaloc Dev <85583693+ZeusXaloc-Dev@users.noreply.github.com>
2021-06-10 18:42:14 -0400

    Add files via upload

---
+++ OperaçãoMatemática.c
@@ -0,0 +1,51 @@
+/*********************************
+  
+    Olá, Me Chamo Zeus Xaloc,
+    Fiz Duas Pequenas Operações
+    Matemáticas na Language C,
+    Veja os dois Tipos de Operações.
+  
+ **********************************/
+
+#include <stdio.h>
+
+//Operações Matemáticas 
+
+int main()
+{
+   //Operação de Calculo!
+    int minhaIdade = 16;  
+    int paiIdade = 36;
+    int maeIdade = 35;  
+    int idadeTotal;
+    
+    idadeTotal = minhaIdade + paiIdade + maeIdade;                          
+  
+    //Resultado da Operação!
+     printf ("Total: %i\n", idadeTotal);
+     printf ("Resultado Calculado!");
+     
+     //Segunda Operação de Cálculo!
+     int tioIdade = 37;
+     int tiaIdade = 36;
+     int voIdade = 57;
+     int IdadeTotal;
+     
+     IdadeTotal = 37 + 36 - 57;
+     
+     //Resultado da Segunda Operação!
+     printf("\nTotal: %i\nResultado Calculo!",IdadeTotal);
+     
+  return 0;
+  
+ 
+  /**************IMPORTANTE!*******************
+    
+      Eu poderia Ter Utilizado na
+      Primeiro Operação o Nome da
+      Variável, Mais eu Utilizei o
+      Valor da Variável como Fiz na
+      Segunda Operação Matemática!
+    
+    *************IMPORTANTE!******************/
+}
